61939fd43ad007c4f616e8e30a4a5a29ff6d587c
hieptv11 <hieptv11@viettel.com.vn>
2024-05-07 09:19:28 +0700

    feat: build success android

--- app/src/main/java/com/example/androidappmodule/Application.kt
+++ app/src/main/java/com/example/androidappmodule/Application.kt
@@ -1,13 +1,15 @@
 package com.example.androidappmodule
 
-import android.content.Context
+import android.app.Application
 import android.support.multidex.MultiDex
-import android.support.multidex.MultiDexApplication
+import com.google.firebase.FirebaseApp
+import com.google.firebase.installations.FirebaseInstallations
 
-
-class MyApplication : MultiDexApplication() {
-    override fun attachBaseContext(context: Context?) {
-        super.attachBaseContext(context)
+class MyApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
         MultiDex.install(this)
+        FirebaseApp.initializeApp(this);
+        FirebaseInstallations.getInstance()
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,18 +11,24 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        repositories {
-            maven {
-                url = uri("${rootDir}/super_app/libs/repo")
-            }
-            maven {
-                url = uri("https://jitpack.io")
-            }
-            maven {
-                url = uri("$storageUrl/download.flutter.io")
-            }
+    }
+
+    repositories {
+        maven {
+            url = uri("${rootDir}/super_app/libs/repo")
+        }
+        maven {
+            url = uri("https://jitpack.io")
+        }
+        maven {
+            url = uri("$storageUrl/download.flutter.io")
         }
     }
+
+//    repositories {
+//        maven(url = "https://storage.googleapis.com/download.flutter.io")
+//        maven(url = "some/path/flutter_module_project/build/host/outputs/repo")
+//    }
 }
 
 rootProject.name = "AndroidAppModule"
